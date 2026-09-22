b973e5064b88faf62d77593d6695aee5e6bc8a01
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 <dani@DESKTOP-0KJM7IA.localdomain>
2026-09-22 10:15:51 +0200

    Estructura inicial del curso y proyecto 01_entrono_vite

---
+++ apuntes/01_entrono_vite/src/main.js
@@ -0,0 +1,30 @@
+import './style.css'
+import heroImg from './assets/hero.png'
+import javascriptLogo from './assets/javascript.svg'
+import viteLogo from './assets/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+<section id="center">
+  <div class="hero">
+    <img src="${heroImg}" class="base" width="170" height="179">
+    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
+    <img src="${viteLogo}" class="vite" alt="Vite logo" />
+  </div>
+  <div>
+    <h1>Get started</h1>
+    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
+  </div>
+  <button id="counter" type="button" class="counter"></button>
+</section>
+
+<div class="ticks"></div>
+
+<section id="next-steps">
+</section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
+`
+
+setupCounter(document.querySelector('#counter'))
---
+++ apuntes/01_entrono_vite/src/tipo_dato.js
@@ -0,0 +1,45 @@
+    //funcion que le pasa como paramtro un numero de grados celsius y lo tranformado a grado kevin 
+
+
+    fuction celsiusToKelvin(celsius){
+      let kelvin = celsius + 273.15
+      return kelvin
+    }
+//v2 version donde priorizamos el el menor numero de lineas 
+
+    function celsiusToKelvin2(celsius){
+      return celsius + 273.15
+    }
+
+
+//v3 (modo pro usando arrow fuction)
+    
+
+    const celToket = (celsius) => {
+
+      return celsius +273.15
+    }
+
+//v4 (modo pro max)
+
+  
+    const celsiusToKelvin1 = (celsius) => c * 273.15
+
+//funcion que le pase como parametro 2 numeros y que me los ordene 
+
+
+  fuction 
+
+
+// funcion que pase de ce celsius a kelvin pero cmprobando que celsius es un numero, que la temperatura no puede estar porr debajo del 0 absoluto -273 grados celsius
+// y el resultado me lo das en 2 cifra decimal
+// isNaN
+// como trincar un numero a 2 cifras 
+  if(){
+
+
+  }else{
+  }
+     w
+    
+    
